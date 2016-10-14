d95f0faec1da3b27dc40d82db778eb1c42937e47
Michael Whalen <mwhalen@farmlogs.com>
2016-10-14 08:48:27 -0400

    Removed dynamic project name #fail

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 
-import os
-import sys
-
 from setuptools import setup
 
 with open('requirements.txt') as f:
   required = f.read().splitlines()
 
-NAME = os.path.split(os.path.dirname(os.path.realpath(__file__)))[1]
+NAME = 'fldb'
 VERSION = '0.0.1'
 
 setup(**{
